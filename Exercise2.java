22726ae69d08b93900f89039218ae9c52313830a
zyij99 <91389500+zyij99@users.noreply.github.com>
2022-04-17 17:33:10 -0400

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -1,4 +1,5 @@
 /*
+Names: Brandon Zheng & Justin Truong
  Exercise 2: Let N (all natural numbers) -> N defined by f(n) = n/2 if n is even, 3n+1 if n is odd
  for every n there is an int i such that f^i(n) = 1. Verify this to be true for n =22 and n = 23
  */
